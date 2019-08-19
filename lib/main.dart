d1edac53381f9a91e1cb78edb619bbd2a24e5a84
Miguel Gray <miguel.gray95@gmail.com>
2019-08-18 21:21:51 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Montserrat-Black'),
+      debugShowCheckedModeBanner: false, // turn off the banner on debug mode
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.red[50],
+            Colors.redAccent[100],
+//            Colors.deepOrange[800],
+//            Colors.red[700],
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          tileMode: TileMode.clamp,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      color: Colors.white,
+                      iconSize: 35.0,
+                      onPressed: () {},
+                    ),
+                    Image.asset(
+                      'images/FireLogo.png',
+                      height: 48.0,
+                      width: 48.0,
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.search),
+                      color: Colors.white,
+                      iconSize: 35.0,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 50.0, right: 12.0, top: 30.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Image.asset(
+                      'images/logo.png',
+                      color: Colors.white,
+                      width: 300.0,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 75.0, right: 12.0, top: 15.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Spellcraft",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 45.0, right: 0.0, top: 15.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'All your favorite spells, in one location.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 35.0, right: 0.0, top: 15.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 300,
+                      child: TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'e.g. ray of frost',
+                          hintStyle: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Montserrat-Black'),
+                          icon: Icon(Icons.arrow_forward_ios),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 100.0, right: 0.0, top: 30.0, bottom: 8.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      height: 50,
+                      child: RaisedButton(
+                        color: Colors.blueAccent[400],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0),
+                        ),
+                        onPressed: () {},
+                        padding: const EdgeInsets.all(0.0),
+                        child: Text(
+                          "Search",
+                          style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 1.0,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
